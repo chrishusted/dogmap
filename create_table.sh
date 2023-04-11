eb463491c5ee2a508ea93660cdca28eed36a09f7
Chris Husted <51458589+chrishusted@users.noreply.github.com>
2023-04-11 18:38:49 -0400

    Update create_table.sh

--- create_table.sh
+++ create_table.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+source /broad/software/scripts/useuse
+use UGER
+use .samtools-1.9
 
 # Check if the input BAM file is provided
 if [ $# -ne 1 ]; then
@@ -7,7 +10,6 @@ if [ $# -ne 1 ]; then
 fi
 
 input_bam_file=$1
-temp_file="temp_output.txt"
 output_file="output.txt"
 
 # Check if the input file exists
@@ -22,20 +24,11 @@ bam_basename=$(basename "$input_bam_file" .bam)
 # Set the output directory
 output_dir="/seq/vgb/chusted/SamToFastq/$bam_basename"
 
-# Empty the temporary output file or create it if it doesn't exist
-> $temp_file
-
-# Execute samtools command, add the basename to the output, and append it to the temporary output file
-samtools view -H "$input_bam_file" | grep '^@RG' | awk -v basename="$bam_basename" -v output_dir="$output_dir" '{ for (i=1;i<=NF;i++) { if ($i ~ /^ID:/) { sub(/^ID:/,"",$i); id=$i; } else if ($i ~ /^LB:/) { sub(/^LB:/,"",$i); lb=$i } } printf "%s\t%s\t$
-    
 # Empty the final output file or create it if it doesn't exist
 > $output_file
 
-# Read each line of the temporary output file, duplicate it, and append it to the final output file
-while read -r line; do
-    echo "$line" >> $output_file
-    echo "$line" >> $output_file
-done < "$temp_file"
+# Execute samtools command, create the desired output, and append it to the final output file
+samtools view -H "$input_bam_file" | grep '^@RG' | awk -v basename="$bam_basename" -v output_dir="$output_dir" '{ for (i=1;i<=NF;i++) { if ($i ~ /^ID:/) { sub(/^ID:/,"",$i); id=$i; } else if ($i ~ /^LB:/$
 
-# Clean up the temporary file
-rm $temp_file
+
+
